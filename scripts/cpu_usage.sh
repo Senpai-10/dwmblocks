4bf1aeb6c467351e319293bc0e0f0a93079f9fd0
senpai-10 <bmjfdrh@gmail.com>
2021-04-12 05:08:14 +0300

    cpu_usage

--- scripts/cpu_usage.sh
+++ scripts/cpu_usage.sh
@@ -1,2 +1,3 @@
-usage=$(top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-echo $usage | perl -nl -MPOSIX -e 'print floor($_)'
+cpu_usage=$(top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+cpu_usage=$(echo $cpu_usage | perl -nl -MPOSIX -e 'print floor($_)')
+echo "[CPU  $cpu_usage%]"
